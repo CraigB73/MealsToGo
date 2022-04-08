554cee89933ee779feffca93eeaeb3586999c016
CraigB73 <craigbritton73@gmail.com>
2022-04-08 11:33:30 +0200

    comminting using command line

--- App.js
+++ App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import 'react-native-gesture-handler';
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
 
-
 import { ThemeProvider } from 'styled-components/native';
 import { theme } from './src/infrastructure/theme';
 
@@ -10,12 +9,12 @@ import { FontAwesome5 } from '@expo/vector-icons';
 import { useFonts as useOswald, Oswald_400Regular } from '@expo-google-fonts/oswald';
 import { useFonts as useLato, Lato_400Regular, Lato_700Bold } from '@expo-google-fonts/lato';
 
-
 import { RestaurantContextProvider } from './src/services/restaurants.context'
 import { LocationContextProvider } from './src/services/location/location.context';
 import { FavouritesContextPovider } from './src/services/favorites/favourites.context';
 import { Navigation } from './src/infrastructure/navigation/index';
 
+import * as firebase from 'firebase/app';
 
 export default function App() {
 
